caac9da04e4e26f2e826cfaaa2d3b9bdbd6d6f6a
Rajan <64787682+rajankm991@users.noreply.github.com>
2022-10-10 20:27:31 +0530

    initial

---
+++ rajanm/script.js
@@ -0,0 +1,30 @@
+//alert("Hello");
+//  document.write("Hello");
+/* Background Gradient for Analagous Colors 
+const text = document.getElementById('text');
+const originalText = text.innerHTML
+const input = document.getElementById('input');
+const button = document.getElementById('button');
+
+
+function highlightWord(){
+    if(input.value){
+        const word= input.value.trim()
+        const regexp = new RegExp(word, 'g')
+        text.innerHTML= originalText.replace(regexp,'<mark>${word}</mark>');
+    }
+    }
+button.addEventListener('click',highlightWord)
+
+
+
+const p = document.getElementById("p");
+const q = document.getElementById("p1");
+function search(){
+    let input = document.getElementById("mysearch").value;
+    if(input !== ""){
+        let regExp = new RegExp(input, "gi");
+        p.innerHTML= (p.textContent).replace(regExp,"<mark>$&</mark>");
+        p1.innerHTML= (p1.textContent).replace(regExp,"<mark>$&</mark>");
+    }
+    }*/
